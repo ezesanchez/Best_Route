51220495c948f449808f16f66e2e4f233bfc5f6b
Ezequiel Sánchez <ezeasanchez@gmail.com>
2022-07-04 21:15:04 +0200

    Distance Europe

--- Distance_Logistics/code/Distance_Europa.py
+++ Distance_Logistics/code/Distance_Europa.py
@@ -1,5 +1,4 @@
-from typing import List
-
+# IMPORTAR LIBRERIAS
 import pandas as pd
 import requests #to get the distances from the API
 import json #to read the API response
@@ -9,39 +8,34 @@ sys.modules['sklearn.externals.six'] = six
 import mlrose #for travelling salesman problem
 import datetime
 
-# https://github.com/vaclavdekanovsky/data-analysis-in-examples/blob/master/Maps/Driving%20Distance/Driving%20Distance%20between%20two%20places.ipynb
-
-
-# load the dataframe with capitals
-df = pd.read_csv("C:\Ezequiel_Sanchez\Proyectos\S&T_Distance_Logistics\data\concap.csv")
+# CARGAR LOS DATOS DE LAS CIUDADES Y SUS COORDENADAS
+df = pd.read_csv("C:\Ezequiel_Sanchez\Proyectos\Distance_Logistics\data\concap.csv", sep=',')
 pd.set_option('display.max_columns', None)
-# print(df)
-# rename so that the column names are shorter and comply with PEP-8
+#print(df)
+
+# RENOMBRAR COLUMNAS EN FORMATO PEP-8
 df.rename(columns={"CountryName": "Country", "CapitalName": "capital", "CapitalLatitude": "lat", "CapitalLongitude": "lon", "CountryCode": "code", "ContinentName": "continent"}, inplace=True)
-# print(df)
+#print(df)
 
-# to start with let's filter only 2 capitals. Rome and Paris.
-# ropa = df[df["capital"].isin(["Rome","Paris"])].reset_index()
-# cities = ropa.copy()
-# print(cities)
-
-# filter only the capitals of the Central Europe
+# FILTRAR 9 CIUDADES DE LA LISTA
 ce_countries = ["AT","CZ","DE","HU","LI","PL","SK","SI","CH"]
+#print(ce_countries)
+
+# CREAR UN NUEVO DF CON LAS CIUDADES SELECCIONADAS
 ce_cities = df[df["code"].isin(ce_countries)].reset_index(drop=True)
-print(ce_countries)
+print("\nCIUDADES SELECCIONADAS")
 print(ce_cities)
 
-
+# OBTENER DISTANCIAS ENTRE LAS CIUDADES SELECCIONADAS CON OpenStreetMap (mapa de uso libre) & API de OSRM
+# http://project-osrm.org/docs/v5.10.0/api/#nearest-service
 def get_distance(point1: dict, point2: dict) -> tuple:
-    # Gets distance between two points en route using http://project-osrm.org/docs/v5.10.0/api/#nearest-service
     url = f"""http://router.project-osrm.org/route/v1/driving/{point1["lon"]},{point1["lat"]};{point2["lon"]},{point2["lat"]}?overview=false&alternatives=false"""
     r = requests.get(url)
-    # get the distance from the returned values
+    # OBTENER LAS DISTANCIAS EN FORMATO JSON
     route = json.loads(r.content)["routes"][0]
     return (route["distance"], route["duration"])
 
-
-# get the distances and durations
+# OBTENER LAS DISTANCIAS Y LAS DURACIONES DE LAS COMBINACIONES SELECCIONADAS
 dist_array = []
 for i , r in ce_cities.iterrows():
     point1 = {"lat": r["lat"], "lon": r["lon"]}
@@ -51,52 +45,68 @@ for i , r in ce_cities.iterrows():
         #dist = geodesic((i_lat, i_lon), (o["CapitalLatitude"], o["CapitalLongitude"])).km
         dist_array.append((i, j, duration, dist))
 
-
+# CREAR DF CON DISTANCIAS Y DURACIONES
 distances_df = pd.DataFrame(dist_array,columns=["origin","destination","duration(s)","distnace(m)"])
 print("DISTANCIAS ENTRE CAPITALES")
 print(distances_df)
 
-# turn the first three columns of the dataframe into the list of tuples
+# TRANSFORMAR LAS TRES PRIMERAS COLUMNAS DEL DF EN UNA LISTA DE TUPLAS
+# Las tuplas se utilizan para almacenar varios elementos en una sola variable
+# IR DEL PUNTO A AL B Y SU DURACION
 dist_list = list(distances_df[["origin","destination","duration(s)"]].sort_values(by=["origin","destination"]).to_records(index=False))
 dist_list[:5] + ["..."] + dist_list[-5:]
-print("")
-print(dist_list)
+#print("\nTUPLAS")
+#print(dist_list)
+#print(dist_array)
 
+# ML CON MLROSE
+# MLROSE es un paquete de Python para aplicar algunos de los algoritmos de búsqueda y optimización aleatoria más comunes
+# a una variedad de problemas de optimización diferentes, tanto en espacios de parámetros de valores discretos como continuos
+# https://mlrose.readthedocs.io/en/stable/
 
-# we plan to use the list of distnaces (durations in our case), that's why we initialize with `distances = dist_list` param.
-fitness_dists = mlrose.TravellingSales(distances = dist_array)
+# OBJETIVO: ENCONTRAR EL RECORRIDO MAS CORTO
+# SELECCIONAR UN ALGORITMO DE OPTIMIZACION ALEATORIA PARA RESOLVER EL PROBLEMA
+# ALGORITMOS GENETICOS
+# Se adaptan para resolver problemas de maximizacion o minimizacion
 
-# we plan to visit 9 cities
+# Fitness -> evolucion (https://www.youtube.com/watch?v=K88hTnzo-tI)
+# TravellingSales() calcula la duracion total de un recorrido
+fitness_dists = mlrose.TravellingSales(distances=dist_array)
+
+# TSPOpt() FUNCION DE OPTIMIZACION PARA LAS CIUDADES SELECCIONADAS
 length = ce_cities.shape[0]
-problem_fit = mlrose.TSPOpt(length = length, fitness_fn = fitness_dists, maximize=False)
+problem_fit = mlrose.TSPOpt(length=length, fitness_fn=fitness_dists, maximize=False)
 
-# non-ideal solution, without specifying mlrose optimization
-mlrose.genetic_alg(problem_fit, random_state = 2)
+# OPCION 1: SIN MODIFICAR LOS HIPERPARAMETROS DE MLROSE
+#best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 2)
+#print("\nSOLUCION")
+#print(best_state)
+#print(best_fitness)
 
-# better but more resource intensive solutions
-best_state, best_fitness = mlrose.genetic_alg(problem_fit, mutation_prob = 0.2,  max_attempts = 500, random_state = 2)
-print("")
-print(f"The best state found is: {best_state}, taking 43hs ({best_fitness} o {str(datetime.timedelta(seconds=best_fitness))})")
+# OPCION 2: MODIFICANDO HIPERPARAMENTROS DE MLROSE
+# Se obtienen mejores resultados, pero se utilizan mas recursos
+best_state, best_fitness = mlrose.genetic_alg(problem_fit, mutation_prob=0.2,  max_attempts=500, random_state=2)
+print("\nSOLUCION")
+print(f"\nMEJOR RECORRIDO ENCONTRADO: {best_state} \nDURACION: 198,7 hs - {best_fitness} segundos \nDIAS: {str(datetime.timedelta(seconds=best_fitness))} horas")
+print("\nSOLUCION GOOGLE MAPS")
+print("DURACION: 190 hs")
 
-
-# turn the results to an ordered dict
+# ORDENAR LOS RESULTADOS
 orders = {city: order for order, city in enumerate(best_state)}
-print("")
-print(orders)
+#print("")
+#print(orders)
 
-# apply this order to the dataframe with the cities
+# CREAR DF CON LA MEJOR RUTA
 ce_cities["order"] = ce_cities.index.map(orders)
-ce_cities = ce_cities.sort_values(by="order")
+ce_cities1 = ce_cities.sort_values(by="order")
 print("")
-print("SHORTEST ROUTE")
-print(ce_cities)
-
-
+print("MEJOR RUTA")
+print(ce_cities1)
 
 
 # GRAFICO
 import plotly.graph_objects as go
-path_df = ce_cities
+path_df = ce_cities1.reset_index()
 # draw the capitals
 fig = go.Figure(data=go.Scattergeo(
     locationmode='USA-states',
